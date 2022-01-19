7ad2e88e5169fb9d9f5faa426cb6557bef84592d
Tadeusz Kleszcz <tkleszcz@silenteight.com>
2022-01-19 18:35:13 +0100

    replace with value in terraform

--- docker/docker-pre-entrypoint.sh
+++ docker/docker-pre-entrypoint.sh
@@ -1,12 +1,12 @@
 #!/bin/dumb-init /bin/bash
 set -e -o pipefail
 
-if [[ -n "$GIT_REPOSITORY" && -n "$ATLANTIS_GITLAB_USERNAME" && -n "$ATLANTIS_GITLAB_TOKEN" ]]; then
+if [[ -n "$GIT_REPOSITORY" && -n "$ATLANTIS_GITLAB_USER" && -n "$ATLANTIS_GITLAB_TOKEN" ]]; then
     echo "Configuring AWS SSM Git credential helper for Git repository: $GIT_REPOSITORY"
 
     git config --global \
         "credential.$GIT_REPOSITORY.helper" \
-        '!bash -c "echo \"username=$ATLANTIS_GITLAB_USERNAME\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
+        '!bash -c "echo \"username=$ATLANTIS_GITLAB_USER\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
 
     echo "AWS SSM Git credential helper configured"
 fi
