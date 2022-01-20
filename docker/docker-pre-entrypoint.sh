7d3e114c3067fb0cd8dd8fbdaed1c1c61ba53064
Andrzej Haczewski <ahaczewski@silenteight.com>
2022-01-20 12:02:28 +0100

    fix: prepend ATLANTIS_GITLAB_HOSTNAME with https://

--- docker/docker-pre-entrypoint.sh
+++ docker/docker-pre-entrypoint.sh
@@ -5,7 +5,7 @@ if [[ -n "$ATLANTIS_GITLAB_HOSTNAME" && -n "$ATLANTIS_GITLAB_USER" && -n "$ATLAN
     echo "Configuring AWS SSM Git credential helper for Git repository: $GIT_REPOSITORY"
 
     git config --global \
-        "credential.$ATLANTIS_GITLAB_HOSTNAME.helper" \
+        "credential.https://$ATLANTIS_GITLAB_HOSTNAME.helper" \
         '!bash -c "echo \"username=$ATLANTIS_GITLAB_USER\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
 
     echo "AWS SSM Git credential helper configured"
