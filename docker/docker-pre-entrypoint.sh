a4218c4d229ec2f86bb8f499acfd3a344a82e073
Andrzej Haczewski <ahaczewski@silenteight.com>
2022-01-20 14:47:05 +0100

    fix: configure git for atlantis user

--- docker/docker-pre-entrypoint.sh
+++ docker/docker-pre-entrypoint.sh
@@ -1,12 +1,18 @@
 #!/bin/dumb-init /bin/bash
 set -e -o pipefail
 
-if [[ -n "$ATLANTIS_GITLAB_HOSTNAME" && -n "$ATLANTIS_GITLAB_USER" && -n "$ATLANTIS_GITLAB_TOKEN" ]]; then
+if [[ -n "$ATLANTIS_GITLAB_HOSTNAME" ]] && [[ -n "$ATLANTIS_GITLAB_USER" ]] && [[ -n "$ATLANTIS_GITLAB_TOKEN" ]]; then
     echo "Configuring AWS SSM Git credential helper for Git repository: $ATLANTIS_GITLAB_HOSTNAME"
 
-    git config --global \
-        "credential.https://$ATLANTIS_GITLAB_HOSTNAME.helper" \
-        '!bash -c "echo \"username=$ATLANTIS_GITLAB_USER\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
+    if [[ $(id -u) == 0 ]]; then
+        gosu atlantis git config --global \
+            "credential.https://$ATLANTIS_GITLAB_HOSTNAME.helper" \
+            '!bash -c "echo \"username=$ATLANTIS_GITLAB_USER\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
+    else
+        git config --global \
+            "credential.https://$ATLANTIS_GITLAB_HOSTNAME.helper" \
+            '!bash -c "echo \"username=$ATLANTIS_GITLAB_USER\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
+    fi
 
     echo "AWS SSM Git credential helper configured"
 fi
