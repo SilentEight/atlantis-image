b7814a25c1bffcb0b8e3beeace19fc01abbe2aa4
Piotr Gralinski <pgralinski@silenteight.com>
2022-01-20 09:13:01 +0100

    feat: reuse Atlantis Gitlab repository env variable in script

--- docker/docker-pre-entrypoint.sh
+++ docker/docker-pre-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/dumb-init /bin/bash
 set -e -o pipefail
 
-if [[ -n "$GIT_REPOSITORY" && -n "$ATLANTIS_GITLAB_USER" && -n "$ATLANTIS_GITLAB_TOKEN" ]]; then
+if [[ -n "$ATLANTIS_GITLAB_HOSTNAME" && -n "$ATLANTIS_GITLAB_USER" && -n "$ATLANTIS_GITLAB_TOKEN" ]]; then
     echo "Configuring AWS SSM Git credential helper for Git repository: $GIT_REPOSITORY"
 
     git config --global \
-        "credential.$GIT_REPOSITORY.helper" \
+        "credential.$ATLANTIS_GITLAB_HOSTNAME.helper" \
         '!bash -c "echo \"username=$ATLANTIS_GITLAB_USER\"; echo \"password=$ATLANTIS_GITLAB_TOKEN\""'
 
     echo "AWS SSM Git credential helper configured"
